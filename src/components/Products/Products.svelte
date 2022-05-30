b87adee1e99fc349064733bf3fb6db2ee96ca16f
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-30 22:48:57 +0300

    added a loading component when data fetching takes a while

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -3,6 +3,7 @@
     import { onMount, onDestroy } from 'svelte';
     import products from '../../stores/defaultProducts';
     import Product from './Product.svelte';
+    import Loading from '../Loading.svelte';
 
     //let localProducts = [];
     // let unsubscribe;
@@ -18,13 +19,13 @@
     // })
 </script>
 
-<h1>Products component</h1>
-
 <section class="section">
     <h2 class="section-title">{title}</h2>
     <div class="products-center">
         {#each $products as product (product.id)}
             <Product {product} />
+        {:else}
+            <Loading />
         {/each}
     </div>
 </section>
