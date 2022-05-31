443619ba18385a5af2212435f80588518f9ef7b2
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-31 16:03:43 +0300

    added basic structure of the navbar and created a small navbar component

---
+++ src/components/Navbar/SmallNavbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    //cart button
+    //global store
+    import {link} from 'svelte-routing';
+</script>
+
+<nav class="navbar">
+    <div class="nav-center">
+        <button class="btn-sidebar-toggle">
+            <i class="fas fa-bars" />
+        </button>
+        <a href="/" use:link class="nav-logo">
+            <img src="/assets/images/logo.svg" class="logo" alt="razors logo">
+        </a>
+        <button>cart</button>
+    </div>
+</nav>
