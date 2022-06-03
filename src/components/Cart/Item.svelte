07339a9eca0c1d5e219304e0fa680d4425c54ce7
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-06-03 21:33:15 +0300

    added styling and buttons to single cart item

--- src/components/Cart/Item.svelte
+++ src/components/Cart/Item.svelte
@@ -1,5 +1,32 @@
 <script>
-
+    export let id;
+    export let image;
+    export let price;
+    export let title;
+    export let amount;
+    // import functionality
 </script>
 
-<h2>single item</h2>
+<div class="cart-item">
+    <img src={image} alt={title}>
+    <div>
+        <h4>{title}</h4>
+        <h5>{price}</h5>
+        <button class="cart-btn remove-btn" on:click={() => {
+            console.log('remove item');
+        }}>remove</button>
+    </div>
+    <div>
+        <button class="cart-btn amount-btn" on:click={() => {
+            console.log('increase item');
+        }}>
+            <i class="fas fa-chevron-up"></i>
+        </button>
+        <p class="item-amount">{amount}</p>
+        <button class="cart-btn amount-btn" on:click={() => {
+            console.log('decrease item');
+        }}>
+            <i class="fas fa-chevron-down"></i>
+        </button>
+    </div>
+</div>
