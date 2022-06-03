9e47d6b16157713ebb2b49c1938fd4864b2555de
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-06-03 21:01:02 +0300

    added cart store with dummy data and counting items in cart button

--- src/components/Cart/CartButton.svelte
+++ src/components/Cart/CartButton.svelte
@@ -1,5 +1,10 @@
 <script>
     import globalStore from '../../stores/globalStore';
+    import cartStore from '../../stores/cart.js';
+    
+    $: total = $cartStore.reduce((prev, next)=>{
+        return (prev += next.amount)
+    }, 0)
 </script>
 
 <div class="btn-cart-container">
@@ -8,5 +13,5 @@
     }}>
         <i class="fas fa-cart-plus"></i>
     </button>
-    <span class="btn-cart-items">10</span>
+    <span class="btn-cart-items">{total}</span>
 </div>
