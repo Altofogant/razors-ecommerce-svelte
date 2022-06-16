6468b141531f96237aa4fd04eac4d6fe1d29fb7f
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-06-16 15:42:00 +0300

    added logout functionality

--- src/components/LoginLink.svelte
+++ src/components/LoginLink.svelte
@@ -1,12 +1,13 @@
 <script>
     import user from '../stores/user';
     import { link } from 'svelte-routing';
-    //logout func
+    import {logoutUser} from '../stores/user';
     import globalStore from '../stores/globalStore';
 </script>
 
 {#if $user.jwt}
     <a href="/" class="logout-btn" on:click={() => {
+        logoutUser();
         globalStore.toggleItem('sidebar', false);
     }}>Logout</a>
 {:else}
