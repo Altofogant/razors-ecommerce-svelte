ce3e49ba6879238d9b0fe9fc20f7063f2de221a4
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-06-19 19:12:19 +0300

    basic setup for the checkout form

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -4,15 +4,35 @@
     import user from '../stores/user';
     import {cartTotal} from '../stores/cart';
 
+    let name = '';
+    $: isEmpty = !name;
+
     onMount(() => {
         if (!$user.jwt) {
             navigate('/');
         }
     });
+
+    function handleSubmit() {
+        console.log('form submitted');
+    }
 </script>
 
 {#if $cartTotal > 0}
-    <h1>form</h1>
+    <section class="form">
+        <h2>checkout</h2>
+        <form action="" class="checkout-form" on:submit|preventDefault={handleSubmit}>
+            <h3>order total: ${cartTotal}</h3>
+            <div class="form-control">
+                <label for="name">your name</label>
+                <input type="text" id="name" bind:value={name}>
+            </div>
+            {#if isEmpty}
+            <p class="form-empty">please fill out name field</p>
+            {/if}
+            <button type="submit" class="btn.btn-block.btn-primary" class:disabled={isEmpty} disabled={isEmpty}>submit</button>
+        </form>
+    </section>
 {:else}
     <div class="checkout-empty">
         <h2>your cart is empty</h2>
